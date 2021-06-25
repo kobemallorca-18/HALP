dbd838c3d6d5896b85260bd499814d4b86d56c61
KaninaRice <bnlapuz@up.edu.ph>
2021-06-26 07:31:26 +0800

    Update register.php

--- registration/register.php
+++ registration/register.php
@@ -8,6 +8,7 @@
 <body>
 <<<<<<< Updated upstream
 <div>
+<<<<<<< Updated upstream
 =======
 
 >>>>>>> Stashed changes
@@ -15,29 +16,38 @@
 	<h2>Register</h2>
 </div>
 
+=======
+>>>>>>> Stashed changes
 <form method="post" action="register.php">
 	<?php echo display_error(); ?>
 <<<<<<< Updated upstream
 </form>
 <form method="post" action="register.php">
+<<<<<<< Updated upstream
 	<div class="input-group">
 =======
 <div class="input-group">
 >>>>>>> Stashed changes
 		<label>Username</label>
 		<input type="text" name="username" value="<?php echo $username; ?>">
+=======
+	
+	<div class="input-group row">
+		<div class="col-2"><label>Username</label></div>
+		<div class="col-8"><input type="text" name="username" value="<?php echo $username; ?>"></div>
+>>>>>>> Stashed changes
 	</div>
 	<div class="input-group">
-		<label>Email</label>
-		<input type="email" name="email" value="<?php echo $email; ?>">
+		<div class="col-2"><label>Email</label></div>
+		<div class="col-8"><input type="email" name="email" value="<?php echo $email; ?>"></div>
 	</div>
 	<div class="input-group">
-		<label>Password</label>
-		<input type="password" name="password_1">
+		<div class="col-2"><label>Password</label>
+		<div class="col-8"><input type="password" name="password_1"></div>
 	</div>
 	<div class="input-group">
-		<label>Confirm password</label>
-		<input type="password" name="password_2">
+		<div class="col-2"><label>Confirm password</label></div>
+		<div class="col-8"><input type="password" name="password_2"></div>
 	</div>
 	<div class="input-group">
 		<button type="submit" class="btn" name="register_btn">Register</button>
