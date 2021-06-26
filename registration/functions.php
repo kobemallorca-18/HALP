bfe29ad1c360d842cc8bcf20f944dbd108135d17
kobemallorca-18 <77627966+kobemallorca-18@users.noreply.github.com>
2021-06-26 09:42:40 +0800

    Update functions.php

--- registration/functions.php
+++ registration/functions.php
@@ -39,7 +39,7 @@ function register(){
 	if ($password_1 != $password_2) {
 		array_push($errors, "The two passwords do not match");
 	}
-
+	else
 	// register user if there are no errors in the form
 	if (count($errors) == 0) {
 		$password = md5($password_1);//encrypt the password before saving in the database
