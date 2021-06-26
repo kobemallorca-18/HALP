9de074e707b1409c4600322154c55c87c7a4fbe5
kobemallorca-18 <77627966+kobemallorca-18@users.noreply.github.com>
2021-06-26 08:40:21 +0800

    Update register.php

--- registration/register.php
+++ registration/register.php
@@ -1,39 +1,45 @@
+<?php include('functions.php') ?>
 <?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+	<title>Registration system PHP and MySQL</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <div class="header">
-  	<h2>Register</h2>
-  </div>
+
+<div class="container-sm">
+
+<div class="header">
+	<h2>Register</h2>
+</div>
+
+<form method="post" action="register.php">
+	<?php echo display_error(); ?>
+	<div class="input-group row">
+		<div class="col-2"><label>Username</label></div>
+		<div class="col-12"><input type="text" name="username" value="<?php echo $username; ?>"></div>
+	</div>
+	<div class="input-group row">
+		<div class="col-2"><label>Email</label></div>
+		<div class="col-12"><input type="email" name="email" value="<?php echo $email; ?>"></div>
+	</div>
+	<div class="input-group row">
+		<div class="col-2"><label>Password</label></div>
+		<div class="col-12"><input type="password" name="password_1" value="<?php echo $password_1; ?>"></div>
+	</div>
+	<div class="input-group row">
+		<div class="col-2"><label>Confirm password</label></div>
+		<div class="col-12"><input type="password" name="password_2" value="<?php echo $password_2; ?>"></div>
+	</div>
+	<div class="input-group mx-auto" style="text-align: center;">
+		<button type="submit" class="btn" name="register_btn">Register</button>
+		<p>
+		Already a member? <a href="login.php">Sign in</a>
+		</p>
+	</div>
 	
-  <form method="post" action="register.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  	  <label>Username</label>
-  	  <input type="text" name="username" value="<?php echo $username; ?>">
-  	</div>
-  	<div class="input-group">
-  	  <label>Email</label>
-  	  <input type="email" name="email" value="<?php echo $email; ?>">
-  	</div>
-  	<div class="input-group">
-  	  <label>Password</label>
-  	  <input type="password" name="password_1">
-  	</div>
-  	<div class="input-group">
-  	  <label>Confirm password</label>
-  	  <input type="password" name="password_2">
-  	</div>
-  	<div class="input-group">
-  	  <button type="submit" class="btn" name="reg_user">Register</button>
-  	</div>
-  	<p>
-  		Already a member? <a href="login.php">Sign in</a>
-  	</p>
-  </form>
+</div>
+</form>
 </body>
 </html>
